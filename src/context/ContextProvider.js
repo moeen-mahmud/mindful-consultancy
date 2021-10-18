50b3f1900f7e83a907c69e95287262492359deb2
Moeen Mahmud <write2fahim007@gmail.com>
2021-10-19 00:25:35 +0600

    refactored context api

--- src/context/ContextProvider.js
+++ src/context/ContextProvider.js
@@ -1,12 +1,19 @@
 import React, { createContext } from "react";
+
+// Importing necessary hooks
 import useFirebase from "../hooks/useFirebase";
 import useServices from "../hooks/useServices";
 
+// Creating Context
 export const ServiceContext = createContext();
 export const AuthContext = createContext();
 
+// Context Provider
 const ContextProvider = ({ children }) => {
+  //Context for showing service data
   const allServiceContext = useServices();
+
+  //Context for user authentication
   const allAuthContext = useFirebase();
   return (
     <AuthContext.Provider value={allAuthContext}>
