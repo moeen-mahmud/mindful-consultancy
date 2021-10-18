62c3375b87551035df4e8124222b93dd90fdb319
Moeen Mahmud <write2fahim007@gmail.com>
2021-10-19 00:53:37 +0600

    refactored reviews component

--- src/Pages/Home/Reviews/Reviews.js
+++ src/Pages/Home/Reviews/Reviews.js
@@ -1,13 +1,17 @@
+// Imporing necessary files
 import React from "react";
 import customer1 from "../../../assets/customer-1.JPG";
 import customer2 from "../../../assets/customer-2.JPG";
 import customer3 from "../../../assets/customer-3.JPG";
 import "./Reviews.css";
 
+// Importing Reviews Component
 const Reviews = () => {
   return (
     <div className="reviews">
       <h1>Our Satisfied Customer Says...</h1>
+
+      {/* Customer Reviews */}
       <div className="user-review">
         <figure>
           <img src={customer1} alt="Susana Kenny" />
